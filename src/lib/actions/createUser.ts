0d6ba28afaff0a9a1fc2a13a69c16586e7565ce0
Yuto Arimori <n01648493@humber.ca>
2025-06-01 17:33:17 -0400

    🐛 Fix createUser

--- src/lib/actions/createUser.ts
+++ src/lib/actions/createUser.ts
@@ -4,15 +4,17 @@ import { registerSchema } from "@/validations/user";
 import { prisma } from "@/lib/prisma";
 import bcryptjs from "bcryptjs";
 import { signIn } from "@/auth";
-import { redirect } from 'next/navigation'
+import { redirect } from 'next/navigation';
+import { ZodError } from "zod";
 
 type ActionState = {
     success: boolean
     errors: Record<string, string[]>
 }
 
-function handleValidationError(error: any): ActionState {
+function handleValidationError(error: ZodError): ActionState {
     const { fieldErrors, formErrors } = error.flatten();
+    const castedFieldErrors = fieldErrors as Record<string, string[]>;
     // zodの仕様でパスワード一致確認のエラーは formErrorsで渡ってくる 
     // formErrorsがある場合は、confirmPasswordフィールドにエラーを追加 
     if (formErrors.length > 0) {
@@ -22,7 +24,7 @@ function handleValidationError(error: any): ActionState {
             }
         }
     }
-    return { success: false, errors: fieldErrors };
+    return { success: false, errors: castedFieldErrors };
 }
 // カスタムエラー処理 
 function handleError(customErrors: Record<string, string[]>): ActionState {
